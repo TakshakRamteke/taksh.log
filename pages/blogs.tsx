9ee056ba011dceed1ce7542d92dc2e0899fabc58
Takshak Ramteke <takshakramteke0708@gmail.com>
2022-02-04 20:41:03 +0000

    Feat : Created the blog page for portfolio.

---
+++ pages/blogs.tsx
@@ -0,0 +1,17 @@
+// The page for all the blogs
+
+import { NextPage } from "next";
+import Link from "next/link";
+
+const Blogs: NextPage = () =>{
+    return (
+        <div>
+            <h1>This will be the page to browse all the blog's </h1>
+            <Link href='/'>
+                <a></a>
+            </Link>
+        </div>
+    )
+}
+
+export default Blogs ;
