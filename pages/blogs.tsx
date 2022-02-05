5eb667ec5a61d09c61a6e7a6dca27fb039d0126f
Takshak Ramteke <takshakramteke0708@gmail.com>
2022-02-05 07:23:11 +0000

    Feat : added query for Blog's.

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,14 +1,54 @@
-// The page for all the blogs
-
-import { NextPage } from "next";
+import { NextPage, GetStaticProps, NextApiResponse } from "next";
 import Link from "next/link";
 
-const Blogs: NextPage = () =>{
+
+type Data = {
+    blogCollection : {
+        items : [
+            tittle : string,
+            slug : string
+        ]
+    }
+  }
+
+export const getStaticProps: GetStaticProps = async () => {
+    const query = `{
+        blogCollection{
+          items{
+            tittle
+            slug
+          }
+        }
+      }`;
+
+    const response: NextApiResponse = await fetch(
+        `https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}/environments/master`,
+        {
+            method : 'POST',
+            headers : {
+                "Content-Type":"application/json",
+                Authorization : `Bearer ${process.env.ACCESSTOKEN}`,
+            },
+            body: JSON.stringify({query})
+        }
+    ).then((res) => res.json());
+
+    console.log(response);
+
+    return {
+        props: {
+            blogs : []
+        },
+    };
+}
+
+const Blogs: NextPage = (blogs) =>{
     return (
         <div>
             <h1>This will be the page to browse all the blog's </h1>
+            <p>{JSON.stringify(blogs)}</p>
             <Link href='/'>
-                <a></a>
+                <a>←</a>
             </Link>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>Taksh.log</title>
+      </Head>
       <div className={styles.container}>
         <h1>This will be Home Page</h1>
         <Link href='/blogs'>
